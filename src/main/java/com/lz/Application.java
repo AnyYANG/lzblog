253e104a7558ff0c02b1f757a05c5e9f7fc8ec3c
liuyang <250935102@qq.com>
2017-09-04 19:21:59 +0800

    applicationjava

---
+++ src/main/java/com/lz/Application.java
@@ -0,0 +1,21 @@
+package com.lz;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 应用启动类
+ *
+ * Created by  lynx  2017/9/4
+ */
+// Spring Boot 应用的标识
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        // 程序启动入口
+        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+        SpringApplication.run(Application.class,args);
+    }
+}
