8e27ce05fec66b527028549cfc10460253dd4836
liuyang <250935102@qq.com>
2017-09-11 21:23:54 +0800

    add new article method
    Signed-off-by: liuyang <250935102@qq.com>

--- src/main/java/com/lz/service/ArticleService.java
+++ src/main/java/com/lz/service/ArticleService.java
@@ -1,5 +1,28 @@
 package com.lz.service;
 
+
+import com.lz.until.FileUntil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
 public class ArticleService {
 
+    /**
+     * 增加一篇新的文章
+     *
+     * @param content 文章内容
+     * @return 返回是否添加成功
+     */
+    public boolean addNewArticle(String username, String content) {
+        if (StringUtils.isNotBlank(content) && StringUtils.isNotBlank(username)) {
+            String title = username + "_" + System.currentTimeMillis();
+            String fileName = FileUntil.CreateFile(title, content);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
