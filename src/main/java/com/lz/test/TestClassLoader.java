14e11f4bee0492c9f163ea8a2c6e541a0a269e31
liuyang <250935102@qq.com>
2017-09-13 18:12:13 +0800

    添加测试模板类
    Signed-off-by: liuyang <250935102@qq.com>

--- src/main/java/com/lz/test/TestClassLoader.java
+++ src/main/java/com/lz/test/TestClassLoader.java
@@ -1,12 +1,21 @@
 package com.lz.test;
 
 import com.lz.Application;
+import com.lz.service.SysUserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+
+import javax.annotation.Resource;
 import java.net.URL;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class TestClassLoader {
+    @Resource
+    private SysUserService sysUserService;
 
     @Test
     public void getClassPath() {
