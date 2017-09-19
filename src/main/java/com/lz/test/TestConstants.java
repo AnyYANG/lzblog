67f02916399b3d0efd697e2f7fcaccafaf4b1caf
刘洋 <250935102@qq.com>
2017-09-19 15:22:16 +0800

    Test Case
    Signed-off-by: liuyang <250935102@qq.com>

---
+++ src/main/java/com/lz/test/TestConstants.java
@@ -0,0 +1,24 @@
+package com.lz.test;
+
+import com.lz.until.Constants;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class TestConstants {
+    @Resource
+    Constants  constants;
+    @Test
+    public void testGetClassPathShellFile(){
+    String path = constants.getShellPath("hexoGenerate.sh");
+    System.out.println(path);
+    Assert.notNull(path,"success");
+    }
+
+}
