37e6e06ddc4a477538e030704cd9b67ce885fcb0
刘洋 <250935102@qq.com>
2017-09-19 17:51:09 +0800

    shell  update
    Signed-off-by: liuyang <250935102@qq.com>

--- src/main/resources/shell/hexoGenerate.sh
+++ src/main/resources/shell/hexoGenerate.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-echo "info: 现在开始更新Hexo工具！";
-rm -rf /usr/local/nginx/html/hexo/zzshexo/source/
-mkdir  /usr/local/nginx/html/hexo/zzshexo/source/_posts
-cp  /root/mywebapp/articleMarkdown/      /usr/local/nginx/html/hexo/zzshexo/source/_posts
+echo "info: startHexo";
+rm -rf /usr/local/nginx/html/hexo/zzshexo/source/_posts
+cp -r  /root/mywebapp/articleMarkdown/article/      /usr/local/nginx/html/hexo/zzshexo/source/_posts
 cd /usr/local/nginx/html/hexo/zzshexo
 hexo  g
 echo "info:update"
-
